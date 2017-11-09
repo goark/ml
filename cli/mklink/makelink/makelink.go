e2fa860359612405db2f4563b81c56b7fda7ff94
Spiegel <spiegel.im.spiegel@gmail.com>
2017-11-09 11:17:46 +0900

    Modified interactive mode

--- cli/mklink/makelink/makelink.go
+++ cli/mklink/makelink/makelink.go
@@ -6,23 +6,28 @@ import (
 	"io"
 
 	"github.com/atotto/clipboard"
+	"github.com/pkg/errors"
 	"github.com/spiegel-im-spiegel/mklink"
 )
 
 //Context class is context for making link
 type Context struct {
-	linkStyle mklink.Style
-	writer    io.Writer
-	log       io.Writer
+	linkStyle   mklink.Style
+	writer      io.Writer
+	log         io.Writer
+	clipbrdFlag bool
 }
 
 //New returns new Context instance
 func New(s mklink.Style, writer io.Writer, log io.Writer) *Context {
-	return &Context{linkStyle: s, writer: writer, log: log}
+	return &Context{linkStyle: s, writer: writer, log: log, clipbrdFlag: false}
 }
 
 //MakeLink is making link
 func (c *Context) MakeLink(url string) error {
+	if c == nil {
+		return errors.New("nil pointer in makelink.Context.MakeLink() function")
+	}
 	lnk, err := mklink.New(url)
 	if err != nil {
 		return err
@@ -32,9 +37,25 @@ func (c *Context) MakeLink(url string) error {
 	buf := new(bytes.Buffer)
 	io.Copy(c.writer, io.TeeReader(r, buf))
 	strLink := buf.String()
-	clipboard.WriteAll(strLink)
+	if c.clipbrdFlag {
+		clipboard.WriteAll(strLink)
+	}
 	if c.log != nil {
 		fmt.Fprint(c.log, strLink)
 	}
 	return nil
+}
+
+//EnableClipboard is enabling to clipboard
+func (c *Context) EnableClipboard() {
+	if c != nil {
+		c.clipbrdFlag = true
+	}
+}
+
+//DisableClipboard is disabling to clipboard
+func (c *Context) DisableClipboard() {
+	if c != nil {
+		c.clipbrdFlag = false
+	}
 }
--- cli/mklink/makelink/makelink_test.go
+++ cli/mklink/makelink/makelink_test.go
@@ -11,6 +11,7 @@ func TestMakeLink(t *testing.T) {
 	outBuf := new(bytes.Buffer)
 	logBuf := new(bytes.Buffer)
 	cxt := New(mklink.StyleMarkdown, outBuf, logBuf)
+	cxt.EnableClipboard()
 	err := cxt.MakeLink("https://git.io/vFR5M")
 	if err != nil {
 		t.Errorf("MakeLink()  = \"%v\", want nil error.", err)
@@ -46,5 +47,20 @@ func TestMakeLinkErr(t *testing.T) {
 	err := cxt.MakeLink("https://foo.bar")
 	if err == nil {
 		t.Error("MakeLink()  = nil error, not want nil error.")
+	}
+}
+
+func TestClipbrdflag(t *testing.T) {
+	cxt := New(mklink.StyleMarkdown, nil, nil)
+	if cxt.clipbrdFlag {
+		t.Errorf("clipbrdFlag  = %v, want false.", cxt.clipbrdFlag)
+	}
+	cxt.EnableClipboard()
+	if !cxt.clipbrdFlag {
+		t.Errorf("clipbrdFlag  = %v, want true.", cxt.clipbrdFlag)
+	}
+	cxt.DisableClipboard()
+	if cxt.clipbrdFlag {
+		t.Errorf("clipbrdFlag  = %v, want false.", cxt.clipbrdFlag)
 	}
 }
